c2bad975f9f4563ec35f85e28c0fe695ab4f9df3
fequme <142742415+Fequme@users.noreply.github.com>
2024-06-17 14:46:30 +0300

    config.py

--- client/config.py
+++ client/config.py
@@ -1,36 +1,36 @@
 SWEETNESS = {
-    "TOKEN": "",
+    "TOKEN": "", # токен вашего бота
     
-    "GUILD_ID": 123, 
-    "MOD_LOGS": 123,
+    "GUILD_ID": 123, # ID вашего сервера
+    "MOD_LOGS": 123, # ID канала с логами
 
     "ACTION_ROLES": {
-        "SWEETNESS": 123,
-        "ADMINISTRATOR": 123,
-        "M_WARDEN": 123,
-        "M_HELPER": 123,
-        "WARDEN": 123,
-        "HELPER": 123
+        "SWEETNESS": 123, # самая высшая роль ID роли
+        "ADMINISTRATOR": 123, # администратор ID роли
+        "M_WARDEN": 123, # кураторы ID роли
+        "M_HELPER": 123, # мастера ID роли
+        "WARDEN": 123, # модераторы ID роли
+        "HELPER": 123 # контролы или ещё раз модераторы ID роли
     },
 
-    "STAFF_ROLE": 123,
+    "STAFF_ROLE": 123, # ID роли персонала
 
     "GENDER_ROLES": {
-        "MALE": 123,
-        "FEMALE": 123
+        "MALE": 123, # ID роли мальчика
+        "FEMALE": 123 # ID роли девочки
     },
 
     "PUNISHMENT_ROLES": {
-        "TEXT_MUTE": 123,
-        "VOICE_MUTE": 123,
-        "BAN": 123
+        "TEXT_MUTE": 123, # ID роли текстового мута
+        "VOICE_MUTE": 123, # ID роли мута в голосовых каналах
+        "BAN": 123 # ID роли бана
     },
 
     "TIME_ROLES": {
-        "TEXT_WARN": 123,
-        "VOICE_WARN": 123,
-        "BAN_WARN": 123
+        "TEXT_WARN": 123, # ID роли предупреждения текстового мута
+        "VOICE_WARN": 123, # ID роли предупреждения голосового мута
+        "BAN_WARN": 123 # ID роли предупреждения бана 
     },
 
-    "MONGO": "",
-}
+    "MONGO": "", # URI MONGO DB
+} 
